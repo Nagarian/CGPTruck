7fa759789cef1858a84431991d962185f0670883
Nagarian47 <onyx_nagarian47@hotmail.fr>
2015-12-05 21:11:24 +0100

    Ajout du projet de test de la DAL

---
+++ CGPTruck.DAL.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CGPTruck.DAL.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            CGPTruck.DAL.DALUsers users = new DALUsers();
+            var user = users.GetUserById(1);
+            Assert.IsNotNull(user);
+        }
+    }
+}
