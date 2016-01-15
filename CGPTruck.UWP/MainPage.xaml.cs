de44f58f5cea058dbefef4a6feb3ec1fdb881e0d
TotoSyfher <ProTechGamer@hotmail.fr>
2016-01-15 16:33:53 +0100

    Modified Navigation system

--- CGPTruck.UWP/MainPage.xaml.cs
+++ CGPTruck.UWP/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CGPTruck.UWP.Views;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -32,6 +33,8 @@ namespace CGPTruck.UWP
             this.InitializeComponent();
         }
 
+        
+
         private async void button_Click(object sender, RoutedEventArgs e)
         {
             //ServiceReference.ServiceClient client = new ServiceReference.ServiceClient();
@@ -40,90 +43,104 @@ namespace CGPTruck.UWP
             bool result = await WebApiService.Current.Authenticate("lala", "tototutu");
         }
 
-        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
-        {
-            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
-        }
-
         private void HamburgerButton_Tapped(object sender, TappedRoutedEventArgs e)
         {
             MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
         }
 
-        private async void MissionButton_Tapped(object sender, TappedRoutedEventArgs e)
+        private void HomeButton_Tapped(object sender, TappedRoutedEventArgs e)
         {
-           Map.TrafficFlowVisible = true;
-           
-
-            for (int i = 0; i < 13; i++)
-            {
-                // Specify a known location.
-                BasicGeoposition snPosition = new BasicGeoposition() { Latitude = 44.853 + i, Longitude = -0.566 + i/2 };
-                Geopoint snPoint = new Geopoint(snPosition);
-
-                // Create a MapIcon.
-                MapIcon mapIcon1 = new MapIcon();
-                mapIcon1.Location = snPoint;
-                mapIcon1.NormalizedAnchorPoint = new Point(0.5, 1.0);
-                mapIcon1.Title = "Mother Fucker " + i;
-                mapIcon1.ZIndex = 0;
-
-                // Add the MapIcon to the map.
-                Map.MapElements.Add(mapIcon1);
-
-                // Center the map over the POI.
-                if(i == 1)
-                    Map.Center = snPoint;
-            }
-            
-            
-            Map.ZoomLevel = 14;
-
-            //await Map.TrySetViewAsync(new Geopoint(new BasicGeoposition() { Latitude = 44.853, Longitude = -0.566 }), 14);
+            frame.Visibility = Visibility.Collapsed;
         }
 
-        private async void TrajetTest(object sender, TappedRoutedEventArgs e)
+        private void ProfileButton_Tapped(object sender, TappedRoutedEventArgs e)
         {
-            // Start at Microsoft in Redmond, Washington.
+            if (frame.CurrentSourcePageType != typeof(Profile))
+                frame.Navigate(typeof(Profile));
 
-            MapService.ServiceToken = "vnevem6H-MEWqD795TAckw";
-            BasicGeoposition startLocation = new BasicGeoposition() { Latitude = 44.85310338370592, Longitude = -0.5674400925636292 };
-
-            // End at the city of Seattle, Washington.
-            BasicGeoposition endLocation = new BasicGeoposition() { Latitude = 44.78670251489458, Longitude = -0.6313490867614746 };
-
-
-            // Get the route between the points.
-            MapRouteFinderResult routeResult =
-                  await MapRouteFinder.GetDrivingRouteAsync(
-                  new Geopoint(startLocation),
-                  new Geopoint(endLocation),
-                  MapRouteOptimization.TimeWithTraffic);
-
-            
-            Map.Routes.Clear();
-
-            if (routeResult.Status == MapRouteFinderStatus.Success)
-            {
-                // Use the route to initialize a MapRouteView.
-                MapRouteView viewOfRoute = new MapRouteView(routeResult.Route);
-                viewOfRoute.RouteColor = Colors.Yellow;
-                viewOfRoute.OutlineColor = Colors.Black;
-
-                MessageDialog msg = new MessageDialog("Duree du trajet: " + routeResult.Route.EstimatedDuration.TotalMinutes.ToString() + " minutes");
-                //await msg.ShowAsync();
-                // Add the new MapRouteView to the Routes collection
-                // of the MapControl.
-                Map.Routes.Add(viewOfRoute);
-
-                // Fit the MapControl to the route.
-                await Map.TrySetViewBoundsAsync(
-                      routeResult.Route.BoundingBox,
-                      null,
-                      Windows.UI.Xaml.Controls.Maps.MapAnimationKind.None);
-            }
-
-            Map.StartContinuousRotate(5);
+            if (frame.Visibility == Visibility.Collapsed)
+                frame.Visibility = Visibility.Visible;
         }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            mapFrame.Navigate(typeof(Map));
+        }
+
+        //private async void MissionButton_Tapped(object sender, TappedRoutedEventArgs e)
+        // {
+        //    Map.TrafficFlowVisible = true;
+
+
+        //     for (int i = 0; i < 13; i++)
+        //     {
+        //         // Specify a known location.
+        //         BasicGeoposition snPosition = new BasicGeoposition() { Latitude = 44.853 + i, Longitude = -0.566 + i/2 };
+        //         Geopoint snPoint = new Geopoint(snPosition);
+
+        //         // Create a MapIcon.
+        //         MapIcon mapIcon1 = new MapIcon();
+        //         mapIcon1.Location = snPoint;
+        //         mapIcon1.NormalizedAnchorPoint = new Point(0.5, 1.0);
+        //         mapIcon1.Title = "Mother Fucker " + i;
+        //         mapIcon1.ZIndex = 0;
+
+        //         // Add the MapIcon to the map.
+        //         Map.MapElements.Add(mapIcon1);
+
+        //         // Center the map over the POI.
+        //         if(i == 1)
+        //             Map.Center = snPoint;
+        //     }
+
+
+        //     Map.ZoomLevel = 14;
+
+        //     //await Map.TrySetViewAsync(new Geopoint(new BasicGeoposition() { Latitude = 44.853, Longitude = -0.566 }), 14);
+        // }
+
+        // private async void TrajetTest(object sender, TappedRoutedEventArgs e)
+        // {
+        //     // Start at Microsoft in Redmond, Washington.
+
+        //     MapService.ServiceToken = "vnevem6H-MEWqD795TAckw";
+        //     BasicGeoposition startLocation = new BasicGeoposition() { Latitude = 44.85310338370592, Longitude = -0.5674400925636292 };
+
+        //     // End at the city of Seattle, Washington.
+        //     BasicGeoposition endLocation = new BasicGeoposition() { Latitude = 44.78670251489458, Longitude = -0.6313490867614746 };
+
+
+        //     // Get the route between the points.
+        //     MapRouteFinderResult routeResult =
+        //           await MapRouteFinder.GetDrivingRouteAsync(
+        //           new Geopoint(startLocation),
+        //           new Geopoint(endLocation),
+        //           MapRouteOptimization.TimeWithTraffic);
+
+
+        //     Map.Routes.Clear();
+
+        //     if (routeResult.Status == MapRouteFinderStatus.Success)
+        //     {
+        //         // Use the route to initialize a MapRouteView.
+        //         MapRouteView viewOfRoute = new MapRouteView(routeResult.Route);
+        //         viewOfRoute.RouteColor = Colors.Yellow;
+        //         viewOfRoute.OutlineColor = Colors.Black;
+
+        //         MessageDialog msg = new MessageDialog("Duree du trajet: " + routeResult.Route.EstimatedDuration.TotalMinutes.ToString() + " minutes");
+        //         //await msg.ShowAsync();
+        //         // Add the new MapRouteView to the Routes collection
+        //         // of the MapControl.
+        //         Map.Routes.Add(viewOfRoute);
+
+        //         // Fit the MapControl to the route.
+        //         await Map.TrySetViewBoundsAsync(
+        //               routeResult.Route.BoundingBox,
+        //               null,
+        //               Windows.UI.Xaml.Controls.Maps.MapAnimationKind.None);
+        //     }
+
+        //     Map.StartContinuousRotate(5);
+        // }
     }
 }
