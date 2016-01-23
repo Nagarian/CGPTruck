91e93d9fc884d3593fe801231d72fca150b71525
Nagarian47 <onyx_nagarian47@hotmail.fr>
2016-01-23 17:44:01 +0100

    désactivation du LazyLoading sur EntityFramework

--- CGPTruck.WebAPI/Entities/EFCGPTruck.Context.cs
+++ CGPTruck.WebAPI/Entities/EFCGPTruck.Context.cs
@@ -18,6 +18,7 @@ namespace CGPTruck.WebAPI.Entities.Entities
         public CGPTruckEntities()
             : base("name=CGPTruckEntities")
         {
+            this.Configuration.LazyLoadingEnabled = false;
         }
     
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
