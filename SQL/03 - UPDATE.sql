9d0649d92e62698508b52dff47f135da4a1e1b41
Nagarian47 <onyx_nagarian47@hotmail.fr>
2016-01-24 02:10:58 +0100

    modification des scripts SQL pour corriger la bourde longitude - latitude

--- SQL/03 - UPDATE.sql
+++ SQL/03 - UPDATE.sql
@@ -1,26 +1,35 @@
-﻿ALTER TABLE Steps
-ADD [Date] DATE NOT NULL;
+﻿USE [CGPTruck]
+--ALTER TABLE Steps
+--ADD [Date] DATE NOT NULL;
+--GO
+
+--ALTER TABLE Users
+--ADD [FirstName] NVARCHAR(50) NOT NULL
+--ALTER TABLE Users
+--ADD [LastName] NVARCHAR(50) NOT NULL 
+--ALTER TABLE Users
+--ADD [Birthday] DATE NOT NULL
+--ALTER TABLE Users
+--ADD [Sexe] BIT NOT NULL DEFAULT(0) 
+--GO
+
+--ALTER TABLE [dbo].[Steps]
+--ALTER COLUMN [Date] DATETIME NOT NULL
+--GO
+
+--ALTER TABLE [dbo].[Failures]
+--ALTER COLUMN [Date] DATETIME NOT NULL
+--GO
+
+---- Ajouter le IDENTITY sur la colonne Id de la table Phones
+
+--ALTER TABLE [dbo].[Failures]
+--DROP COLUMN [Failure_Detail_Id];
+
+UPDATE [Positions]
+SET [Latitude] = [Longitude], [Longitude] = [Latitude]
 GO
 
-ALTER TABLE Users
-ADD [FirstName] NVARCHAR(50) NOT NULL
-ALTER TABLE Users
-ADD [LastName] NVARCHAR(50) NOT NULL 
-ALTER TABLE Users
-ADD [Birthday] DATE NOT NULL
-ALTER TABLE Users
-ADD [Sexe] BIT NOT NULL DEFAULT(0) 
+INSERT INTO Missions(Name, Description, Date, Vehicule_Id, Driver_Id, Pickup_Place_Id, Delivery_Place_Id)
+VALUES ('Livraison - Lactel Bis', 'Livraison des packs de lait Lactel au client "Auchan - Bordeaux Lac"', '2016-02-24 11:10', 1, 1, 10, 8)
 GO
-
-ALTER TABLE [dbo].[Steps]
-ALTER COLUMN [Date] DATETIME NOT NULL
-GO
-
-ALTER TABLE [dbo].[Failures]
-ALTER COLUMN [Date] DATETIME NOT NULL
-GO
-
--- Ajouter le IDENTITY sur la colonne Id de la table Phones
-
-ALTER TABLE [dbo].[Failures]
-DROP COLUMN [Failure_Detail_Id];
