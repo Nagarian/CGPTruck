41edeb5c57eedb66cdd0d46ea854cff8810390bb
Nagarian47 <onyx_nagarian47@hotmail.fr>
2016-01-24 04:34:51 +0100

    ajout d'un projet de test pour tester rapidement les méthodes de la BLL

---
+++ CGPTruck.Test/BLLMissionsTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CGPTruck.WebAPI.BLL;
+
+namespace CGPTruck.Test
+{
+    [TestClass]
+    public class BLLMissionsTest
+    {
+        private BLLMissions missions;
+
+        public BLLMissionsTest()
+        {
+             missions = new BLLMissions();
+        }
+
+        [TestMethod]
+        public void TestGetMissions()
+        {
+            var m = missions.GetMissions();
+
+        }
+
+
+        [TestMethod]
+        public void TestGetActiveMissions()
+        {
+            var m = missions.GetActiveMissions();
+
+        }
+    }
+}
