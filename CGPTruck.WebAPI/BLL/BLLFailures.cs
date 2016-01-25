211d89b4782532b0b23e51b65e245e8d13b0bfb1
Nagarian47 <onyx_nagarian47@hotmail.fr>
2016-01-25 01:51:20 +0100

    ajout des méthodes correspondant aux pannes (BLL + controller)

---
+++ CGPTruck.WebAPI/BLL/BLLFailures.cs
@@ -0,0 +1,38 @@
+﻿using CGPTruck.WebAPI.Entities;
+using CGPTruck.WebAPI.Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CGPTruck.WebAPI.BLL
+{
+    /// <summary>
+    /// Business Logic Layer des pannes
+    /// </summary>
+    public class BLLFailures
+    {
+        /// <summary>
+        /// Permet d'obtenir une instance rapidement
+        /// </summary>
+        public static BLLFailures Current { get; set; } = new BLLFailures();
+
+
+        /// <summary>
+        /// Permet d'obtenir toutes les pannes active de la BDD
+        /// </summary>
+        /// <returns></returns>
+        public List<Failure> GetFailures()
+        {
+            using (CGPTruckEntities context = new CGPTruckEntities())
+            {
+                return (from failure in context.Failures.Include(f => f.Mission.Driver.Phones)
+                                                        .Include(f => f.Vehicule)
+                        where failure.State == FailureState.Declared
+                        select failure).ToList();
+            }
+        }
+    }
+}
---
+++ CGPTruck.WebAPI/Controllers/FailuresController.cs
@@ -0,0 +1,39 @@
+﻿using CGPTruck.WebAPI.BLL;
+using CGPTruck.WebAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+
+namespace CGPTruck.WebAPI.Controllers
+{
+    /// <summary>
+    /// Controller concernant les pannes. Authentification requise
+    /// </summary>
+    [Authorize]
+    public class FailuresController : BaseController
+    {
+        private BLLFailures failures = new BLLFailures();
+
+        // GET: api/Places
+        /// <summary>
+        /// Administrator/DecisionMaker : Obtient la liste de toutes les places
+        /// </summary>
+        /// <returns></returns>
+        [Route("api/Failures/Declared")]
+        [HttpGet]
+        [ResponseType(typeof(List<Failure>))]
+        public IHttpActionResult GetFailures()
+        {
+            if (CurrentUser.AccountType == AccountType.Repairer || CurrentUser.AccountType == AccountType.Driver)
+            {
+                return Unauthorized();
+            }
+
+            return Ok(failures.GetFailures());
+        }
+    }
+}
